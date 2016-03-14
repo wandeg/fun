fd4982ba3820146ef5472d9388c1c7e8e0a13d93
wandeg <jmsmaringa@gmail.com>
2016-03-14 23:07:43 +0300

    Addition if improved algorithm that checks prime factors only

--- fibo_primes.py
+++ fibo_primes.py
@@ -2,13 +2,32 @@ import math
 from utils import func_timer
 from special_nums import get_factors
 
+@func_timer
 def get_primes(lower,upper):
 	"""Gets all the prime numbers between the lower and upper limits provided"""
-	primes = []
+	primes = [2]
 	for i in range(lower,upper):
 		if len(get_factors(i)) == 1:
 			primes.append(i)
 
 	return primes
-print get_primes(3,1000)
-assert get_primes(3,7) == [3,5]
+
+get_primes(3,10000)
+# assert get_primes(3,7) == [2,3,5]
+@func_timer
+def primes_improved(lower,upper):
+	"""Reduces the number of operations by checking against prime factors instead of all possible integer factors"""
+	primes = [2]
+	for i in range(lower,upper):
+		top = math.ceil(math.sqrt(i))
+		for j in primes:
+			if i%j == 0:
+				break
+			elif i%j != 0 and j<top:
+				continue
+			else:
+				primes.append(i)
+				break
+	return primes
+
+primes_improved(3,10000)

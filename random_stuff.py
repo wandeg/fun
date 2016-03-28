e4588879da729565d2e61ab2e553f94b58933b8b
James <jmsmaringa@gmail.com>
2016-03-28 23:01:19 +0300

    Add code to get max odd from input numbers@

--- random_stuff.py
+++ random_stuff.py
@@ -11,3 +11,15 @@ def max_odd(x,y,z):
 		return highest
 	return "Yo!!! please get serious and gimme odd numbers"
 
+def max_odd_input(n):
+	"""Takes n positive integers from the user's input and returns the largest odd number among them"""
+	count = 0
+	highest = 0
+	while count < n:
+		num = int(raw_input("Please insert digit: "))
+		if num>highest and num %2 != 0:
+			highest = num
+		count +=1
+	if highest %2 != 0:
+		return highest
+	return "Yo!!! please get serious and gimme odd numbers"

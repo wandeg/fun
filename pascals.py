c222dbd8550ad9dda4430aab4c08dccc7b8ebe84
James Maringa <james.maringa@gro-intelligence.com>
2016-04-11 22:06:45 +0300

    Naive way to get row in pascals triangle

--- pascals.py
+++ pascals.py
@@ -20,3 +20,32 @@ def factorial_rec(n):
 		return 1
 	else :
 		return n*factorial_rec(n-1)
+
+@func_timer
+def n_choose_k(n,k):
+	"""
+	Returns the number of ways you can choose k 
+	items from n items in any order
+	"""
+
+	return factorial_loop(n)/factorial_loop(k)*factorial_loop(n-k)
+
+@func_timer
+def pascals_triangle_naive(n):
+	"""
+	Returns the nth row of pascals triangle the naive way 
+	by starting from the first row
+	"""
+	arr = [1]
+	if n == 0:
+		return arr
+	for i in xrange(0,n):
+		arr.insert(0,0)
+		arr.append(0)
+		nu_arr = []
+		for i in xrange(len(arr)-1):
+			nu_arr.append(arr[i]+arr[i+1])
+		arr = nu_arr
+
+	return nu_arr
+

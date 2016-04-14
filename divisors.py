53638e4bf6ce3fe0e35704b90931f19376af885c
James Maringa <james.maringa@gro-intelligence.com>
2016-04-14 22:35:44 +0300

    Naive gcd using repeated division

--- divisors.py
+++ divisors.py
@@ -7,9 +7,28 @@ def get_quot_rem(a,b):
 	expresses them as a = qb+r
 	"""
 	c = a
-	assert a%b != 0
+	if a%b == 0:
+		return a/b, 0
 	while c%b != 0:
 		c-=1
 	quot = c/b
 	rem = a-(b*quot)
 	return quot, rem
+
+def gcd_naive(a,b):
+	"""
+	Given 2 positive numbers it returns the gcd
+	of the numbers using the remainder through
+	repeated ddivision
+	"""
+	quot, rem = get_quot_rem(a,b)
+	if rem == 0:
+		return b
+	else:
+		while rem != 0:
+			a = b
+			b = rem
+			quot, rem = get_quot_rem(a,b)
+
+		return b
+
